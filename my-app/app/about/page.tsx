1f07de219ef4d051a720a2deb718f107c12acedb
Syed M.Kumail Ali <m.kumail80@gmail.com>
2025-07-16 17:57:25 +0500

    great the learning of main functionality is done. Moving forward to making the site professional

--- my-app/app/about/page.tsx
+++ my-app/app/about/page.tsx
@@ -4,6 +4,7 @@ const AboutPage = () => {
     return (
         <main>
             <h1>About Us</h1>
+            
             <p>
                 Welcome to our website! We are passionate about building modern web applications with Next.js.
             </p>
